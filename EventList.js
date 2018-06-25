3ca14f65316c7c9cb633da6887a0a74852134641
Juanes Velasquez <juesvema_12@hotmail.com>
2018-06-25 11:14:08 -0500

    Update state each second in EventList Component

--- EventList.js
+++ EventList.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import {
+    StyleSheet,
+    FlatList,
+} from 'react-native';
 import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 5,
+    },
+});
 
 class EventList extends Component {
     state = {
@@ -8,20 +18,27 @@ class EventList extends Component {
     }
 
     componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now(),
+                })),
+            });
+        }, 1000);
         const events = require('./db.json').events.map(e => ({
             ...e,
             date: new Date(e.date),
         }));
-        this.setState({
-            events
-        });
+        this.setState({ events });
     }
-    
+
     render() {
         return (
-            <FlatList 
+            <FlatList
+                style={styles.list}
                 data={this.state.events}
-                renderItem={({ item }) => <EventCard event={item}/>}
+                renderItem={({ item }) => <EventCard event={item} />}
                 keyExtractor={item => item.id}
             />
         );
