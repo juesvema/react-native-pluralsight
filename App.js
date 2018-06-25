e8a3e3c94955989cc83108bf2ee3fda2bf975c45
Juanes Velasquez <juesvema_12@hotmail.com>
2018-06-25 10:45:09 -0500

    First component using FlatList tag

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Pluralsight course</Text>
-      </View>
+      <EventList />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ EventList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { FlatList, Text } from 'react-native';
+
+class EventList extends Component {
+    render() {
+        return (
+            <FlatList 
+                data={[{name: 'a'}, { name: 'b'}]}
+                renderItem={({ item }) => <Text>{item.name}</Text>}
+            />
+        );
+    }
+}
+
+export default EventList;
