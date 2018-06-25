9a47101f4e5e598b242590e256d3b7d5784c23bd
Juanes Velasquez <juesvema_12@hotmail.com>
2018-06-25 11:33:13 -0500

    Use of react navigation, React form component created

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import EventList from './EventList';
+import EventForm from './EventForm';
+import { createStackNavigator } from 'react-navigation'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <EventList />
-    );
-  }
-}
-
+export default createStackNavigator({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your Events',
+    }),
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add an event',
+    }),
+  },
+});
